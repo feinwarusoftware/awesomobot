0b3b8bfce79ad59ee8ad7552d4f4aa3699244ea1
Unknown <lukasznie1320@gmail.com>
2017-12-31 00:47:20 +0000

    muh working data

--- src/web/controllers/dashboard.js
+++ src/web/controllers/dashboard.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const router = express.Router();
 
+const Server = require("../../common/models/server");
+
 router.get("/", (req, res) => {
     res.render("dashboard/index", { user: req.user });
 });
@@ -15,8 +17,15 @@ router.get("/leaderboards", (req, res) => {
     res.render("dashboard/leaderboards", { user: req.user });
 });
 
-router.get("/moderation", (req, res) => {
-    res.render("dashboard/moderation", { user: req.user });
+router.get("/moderation/:server_id", (req, res) => {
+
+    Server.findById(req.params.server_id, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/moderation", { user: req.user, server: server });
+    });
 });
 
 router.get("/developer", (req, res) => {
