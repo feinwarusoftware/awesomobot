85b6781550364c081a7fd688a33a6267568e80a9
Unknown <lukasznie1320@gmail.com>
2017-12-31 18:39:58 +0000

    muh server picker

--- src/web/controllers/dashboard.js
+++ src/web/controllers/dashboard.js
@@ -6,15 +6,40 @@ const router = express.Router();
 const Server = require("../../common/models/server");
 
 router.get("/", (req, res) => {
-    res.render("dashboard/index", { user: req.user });
+    res.render("dashboard/select", { user: req.user });
 });
 
-router.get("/stats", (req, res) => {
-    res.render("dashboard/stats", { user: req.user });
+router.get("/:server_id", (req, res) => {
+    
+    Server.findById(req.params.server_id, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/index", { user: req.user, server: server });
+    });
 });
 
-router.get("/leaderboards", (req, res) => {
-    res.render("dashboard/leaderboards", { user: req.user });
+router.get("/stats/:server_id", (req, res) => {
+    
+    Server.findById(req.params.server_id, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/stats", { user: req.user, server: server });
+    });
+});
+
+router.get("/leaderboards/:server_id", (req, res) => {
+    
+    Server.findById(req.params.server_id, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/leaderboards", { user: req.user, server: server });
+    });
 });
 
 router.get("/moderation/:server_id", (req, res) => {
@@ -28,20 +53,48 @@ router.get("/moderation/:server_id", (req, res) => {
     });
 });
 
-router.get("/developer", (req, res) => {
-    res.render("dashboard/developer", { user: req.user });
+router.get("/developer/:server_id", (req, res) => {
+   
+    Server.findById(req.params.server_id, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/developer", { user: req.user, server: server });
+    });
 });
 
-router.get("/music", (req, res) => {
-    res.render("dashboard/music", { user: req.user });
+router.get("/music/:server_id", (req, res) => {
+    
+    Server.findById(req.params.server_id, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/music", { user: req.user, server: server });
+    });
 });
 
-router.get("/games", (req, res) => {
-    res.render("dashboard/games", { user: req.user });
+router.get("/games/:server_id", (req, res) => {
+    
+    Server.findById(req.params.server_id, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/games", { user: req.user, server: server });
+    });
 });
 
-router.get("/integrations", (req, res) => {
-    res.render("dashboard/integrations", { user: req.user });
+router.get("/integrations/:server_id", (req, res) => {
+    
+    Server.findById(req.params.server_id, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/integrations", { user: req.user, server: server });
+    });
 });
 
 module.exports = router;
