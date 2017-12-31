fb4fa7034af1bfa5f2d511553b0a1d0358cc57ca
Matthew Watt <mattheousdt@users.noreply.github.com>
2017-12-31 20:04:26 +0000

    muh different languages

--- src/web/controllers/index.js
+++ src/web/controllers/index.js
@@ -5,17 +5,24 @@ const checkAuth = require("../middlewares/checkAuth");
 
 const router = express.Router();
 
+// Rest api routes.
 router.use("/api/v1", require("./api"));
 
+// Auth routes.
 router.use("/auth", require("./auth"));
-router.use("/dashboard", checkAuth, require("./dashboard"));
 
+// Dashboard routes en.
+router.use("/dashboard", checkAuth, require("./dashboard"));
+// Dashboard routes other lang.
+router.use("/:lang/dashboard", checkAuth, require("./dashboard"));
+
+// Homepage en.
 router.get("/", (req, res) => {
     res.render("index");
 });
-
-router.get("/nologin", (req, res) => {
-    res.render("nologin");
+// Homepage other lang.
+router.get("/:lang", (req, res) => {
+    res.render("index-" + req.params.lang);
 });
 
 module.exports = router;
