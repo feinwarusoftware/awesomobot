97b06761da55ae1d2ffd845c6646c4bb727f1056
Unknown <lukasznie1320@gmail.com>
2018-01-03 00:30:23 +0000

    muh issue schema

--- src/web/controllers/api/index.js
+++ src/web/controllers/api/index.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 // Server specific routes.
 router.use("/servers", require("./servers"));
+// issue specific routes.
+router.use("/issues", require("./issues"));
 
 module.exports = router;
