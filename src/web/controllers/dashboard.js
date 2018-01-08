9e24d794114eab3a7d8fdcd6ed5597927fc855ef
Unknown <lukasznie1320@gmail.com>
2018-01-08 20:42:45 +0000

    muh deletions

--- src/web/controllers/dashboard.js
+++ src/web/controllers/dashboard.js
@@ -5,11 +5,10 @@ const router = express.Router();
 
 const Server = require("../../common/models/server");
 
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
 
     if (req.user.currentGuild) {
-        res.redirect("/dashboard/home");
-        return;
+        return next();
     }
 
     var queries = [];
@@ -30,13 +29,7 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/home/:server_id", (req, res) => {
-
-    req.user.currentGuild = req.params.server_id;
-    res.redirect("/dashboard/home");
-});
-
-router.get("/home", (req, res) => {
+router.get("/", (req, res) => {
 
     if (!req.user.currentGuild) {
         res.send("You need to select a server first!");
@@ -47,91 +40,23 @@ router.get("/home", (req, res) => {
             res.send(err);
         }
 
-        res.render("dashboard/home", { user: req.user, server: server });
-    });
-});
-
-/*
-router.get("/", (req, res) => {
-
-    var queries = [];
-    for (var i = 0; i < req.user.guilds.length; i++) {
-        queries.push(Server.findById(req.user.guilds[i].id));
-    }
-
-    Promise.all(queries).then(servers => {
-        
-        for (var i = 0; i < servers.length; i++) {
-            req.user.guilds[i].bot = false;
-            if (servers[i] != null) {
-                req.user.guilds[i].bot = true;
-            }
-        }
-
-        res.render("dashboard/select", { user: req.user });
+        res.render("dashboard/", { user: req.user, server: server });
     });
 });
 
 router.get("/:server_id", (req, res) => {
-    
-    Server.findById(req.params.server_id, (err, server) => {
-        if (err) {
-            res.send(err);
-        }
 
-        res.render("dashboard/index", { user: req.user, server: server });
-    });
+    req.user.currentGuild = req.params.server_id;
+    res.redirect("/dashboard");
 });
 
-// TESTING
-router.get("/graph/:server_id", (req, res) => {
+router.get("/developer", (req, res) => {
 
-    Server.findById(req.params.server_id, (err, server) => {
-        if (err) {
-            res.send(err);
-        }
+    if (!req.user.currentGuild) {
+        res.send("You need to select a server first!");
+    }
 
-        res.render("dashboard/graph", { user: req.user, server: server, test: 101 });
-    });
-});
-//
-
-router.get("/stats/:server_id", (req, res) => {
-    
-    Server.findById(req.params.server_id, (err, server) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.render("dashboard/stats", { user: req.user, server: server });
-    });
-});
-
-router.get("/leaderboards/:server_id", (req, res) => {
-    
-    Server.findById(req.params.server_id, (err, server) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.render("dashboard/leaderboards", { user: req.user, server: server });
-    });
-});
-
-router.get("/moderation/:server_id", (req, res) => {
-
-    Server.findById(req.params.server_id, (err, server) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.render("dashboard/moderation", { user: req.user, server: server });
-    });
-});
-
-router.get("/developer/:server_id", (req, res) => {
-   
-    Server.findById(req.params.server_id, (err, server) => {
+    Server.findById(req.user.currentGuild, (err, server) => {
         if (err) {
             res.send(err);
         }
@@ -140,20 +65,13 @@ router.get("/developer/:server_id", (req, res) => {
     });
 });
 
-router.get("/music/:server_id", (req, res) => {
-    
-    Server.findById(req.params.server_id, (err, server) => {
-        if (err) {
-            res.send(err);
-        }
+router.get("/games", (req, res) => {
 
-        res.render("dashboard/music", { user: req.user, server: server });
-    });
-});
+    if (!req.user.currentGuild) {
+        res.send("You need to select a server first!");
+    }
 
-router.get("/games/:server_id", (req, res) => {
-    
-    Server.findById(req.params.server_id, (err, server) => {
+    Server.findById(req.user.currentGuild, (err, server) => {
         if (err) {
             res.send(err);
         }
@@ -162,16 +80,64 @@ router.get("/games/:server_id", (req, res) => {
     });
 });
 
-router.get("/integrations/:server_id", (req, res) => {
-    
-    Server.findById(req.params.server_id, (err, server) => {
+router.get("/leaderboards", (req, res) => {
+
+    if (!req.user.currentGuild) {
+        res.send("You need to select a server first!");
+    }
+
+    Server.findById(req.user.currentGuild, (err, server) => {
         if (err) {
             res.send(err);
         }
 
-        res.render("dashboard/integrations", { user: req.user, server: server });
+        res.render("dashboard/leaderboards", { user: req.user, server: server });
     });
 });
-*/
+
+router.get("/moderation", (req, res) => {
+
+    if (!req.user.currentGuild) {
+        res.send("You need to select a server first!");
+    }
+
+    Server.findById(req.user.currentGuild, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/moderation", { user: req.user, server: server });
+    });
+});
+
+router.get("/music", (req, res) => {
+
+    if (!req.user.currentGuild) {
+        res.send("You need to select a server first!");
+    }
+
+    Server.findById(req.user.currentGuild, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/music", { user: req.user, server: server });
+    });
+});
+
+router.get("/stats", (req, res) => {
+
+    if (!req.user.currentGuild) {
+        res.send("You need to select a server first!");
+    }
+
+    Server.findById(req.user.currentGuild, (err, server) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.render("dashboard/stats", { user: req.user, server: server });
+    });
+});
 
 module.exports = router;
