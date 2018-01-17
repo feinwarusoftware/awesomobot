8203f0de35ed0e57c0beeea592fde52a0a4874bb
Unknown <lukasznie1320@gmail.com>
2018-01-17 21:28:49 +0000

    muh 404

--- src/web/server.js
+++ src/web/server.js
@@ -49,6 +49,21 @@ function start() {
     app.use(passport.session());
     app.use(require("./controllers"));
 
+    app.use((req, res, next) => {
+        res.status(404);
+
+        if (req.accepts("html")) {
+            res.render("404", { url: req.url });
+            return;
+        }
+
+        if (req.accepts("json")) {
+            res.send({ error: "404 not found" });
+        }
+
+        res.type("txt").send("404 not found");
+    });
+
     // Start the webserver.
     app.listen(port, () => {
         console.log("Magic happens on port: " + port);
