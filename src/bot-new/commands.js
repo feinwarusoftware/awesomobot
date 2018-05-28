3d0a5a9ccccda3029a70994d95fba7a918241666
Unknown <lukasznie1320@gmail.com>
2018-05-28 17:46:52 +0100

    better fixes?

--- src/bot-new/commands.js
+++ src/bot-new/commands.js
@@ -3864,12 +3864,6 @@ const commands = [
             const bgWidth = 455;
             const bgHeight = 630;
 
-            while (cardSending === true) {
-                await timeout(200);
-            }
-
-            cardSending = true;
-
             // image overlaying stuff.
             let bg = await new jimp(800, 1200);
             let cardArt = await jimp.read(path.join(__dirname, "assets", "art", "cards", card.art));
@@ -3932,6 +3926,19 @@ const commands = [
             bg.autocrop(0.0002, false);
 
             const date = "test";
+
+            //while (cardSending === true) {
+            //    await timeout(200);
+            //}
+
+            for (let i = 0; i < 5; i++) {
+                if (cardSending === false) {
+                    break;
+                }
+                await timeout(200);
+            }
+
+            cardSending = true;
 
             bg.write(path.join(__dirname, "assets", `${date}.png`), async function() {
 
