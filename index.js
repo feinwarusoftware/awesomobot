ee2f12ee83ac9957b5f9a656f919a36ea6e9b7c5
Dragon1320 <lukasznie1320@gmail.com>
2018-06-16 17:35:35 +0100

    web setup

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const db = require("./db");
-const utils = require("./utils");
-require("./web");
-const bot = require("./bot");
+const utils = require("./src/utils");
 
 utils.hookStderr(write => {
     
@@ -13,54 +10,4 @@ utils.hookStderr(write => {
     }
 });
 
-const test = async () => {
-
-    const mongo = new db.Database("db-test", "mongodb://localhost/bot-awesomo");
-
-    const testScript1 = new db.scriptSchema({
-        author: null,
-        name: "test2",
-        description: "a script for testing bot stuff",
-        type: "javascript",
-        dependencies: [],
-        permissions: [],
-        event: bot.EVENTS.MESSAGE,
-        relay: null,
-        code: "while(true);"
-    });
-
-    const testScript2 = new db.scriptSchema({
-        author: null,
-        name: "test2",
-        description: "a script for testing bot stuff",
-        type: "javascript",
-        dependencies: [],
-        permissions: [],
-        event: bot.EVENTS.MESSAGE,
-        relay: null,
-        code: "while(true);"
-    });
-
-    await mongo.saveScript(testScript1);
-    await mongo.saveScript(testScript2);
-
-    const testGuild = new db.guildSchema({
-        discordId: "405129031445381120",
-        scripts: [ testScript1._id, testScript2._id ]
-    });
-
-    await mongo.saveGuild(testGuild);
-
-    console.log("finished");
-}
-
-const test2 = async () => {
-
-    const awesomo = new bot.Bot("awesomo", "MzcyNDYyNDI4NjkwMDU1MTY5.DgXdnw.y5wKs2e_L2PYgIfn4P5eWg7dksA");
-
-    awesomo.start();
-}
-
-console.error("***awesomo 3.0 wip*** - run 'cd old && node .' to start the old bot");
-
-//test();
+console.error("***AWESOM-O 3.0 WIP*** - The project structure has chaned! Please go to ./cmd/<module_name> and run 'node .' to launch the appropriate module.");
