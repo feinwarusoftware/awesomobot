a3206b8d69b5e1aff82b33148dd1b2f3d1df4346
Dragon1320 <lukasznie1320@gmail.com>
2018-06-17 22:30:28 +0100

    gitignore

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,23 @@
 "use strict";
 
-class Bot {
-    constructor() {
+const path = require("path");
 
+const discord = require("discord.js");
+
+const db = require("./database");
+const log = require("./logger");
+const sb = require("./sandbox");
+const utils = require("./utils");
+
+const cfg = utils.loadJson(path.join(__dirname, "..", "config", "main.json"));
+
+class Bot {
+    constructor(id, token) {
+
+        this.id = id;
+        this.token = token;
+
+        this.client = new discord.Client();
     }
 }
 
