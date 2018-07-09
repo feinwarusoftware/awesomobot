61fb2b76e6a6d3502fbc3537ac07349f31f7441a
Dragon1320 <lukasznie1320@gmail.com>
2018-07-09 14:26:30 +0100

    maybe itll work this time?

--- src/web/index.js
+++ src/web/index.js
@@ -27,6 +27,8 @@ try {
     apiLogger.fatalError(`Could not read config file: ${err}`);
 }
 
+app.enable("trust proxy");
+
 app.use(morgan(config.env === "dev" ? "combined" : "combined", { stream: new stream.Writable({
     write: (chunk, encoding, next) => {
 
